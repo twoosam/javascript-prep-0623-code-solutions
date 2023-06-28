5a578493f142f9268f7d7b59c7f0c4b9482875c3
Tim Woo-Sam <twoosam10@gmail.com>
2023-06-28 00:07:14 +0000

    Changed var to const keyword and added lowercase to array.

--- prep-javascript-loops/main.js
+++ prep-javascript-loops/main.js
@@ -1,45 +1,45 @@
 function whileLoop1() {
-  var Array = [];
+  const array = [];
   let i = 0;
   while (i < 10) {
-    Array.push(i);
+    array.push(i);
     i++;
   }
-  return Array;
+  return array;
 }
 console.log('whileLoop1 output', whileLoop1());
 
 function whileLoop2() {
-  var Array = [];
+  const array = [];
   let i = 0;
   while (i <= 18) {
-    Array.push(i);
+    array.push(i);
     i += 2;
   }
-  return Array;
+  return array;
 }
 console.log('whileLoop2 output', whileLoop2());
 
 function forLoop1() {
-  var Array = [];
+  const array = [];
   for (let i = 0; i < 10; i++) {
-    Array.push(i);
+    array.push(i);
   }
-  return Array;
+  return array;
 }
 console.log('forLoop1 output', forLoop1());
 
 function forLoop2() {
-  var Array = [];
+  const array = [];
   for (let i = 100; i >= 0; i--) {
-    Array.push('Time till explosion' + ' ' + i);
+    array.push('Time till explosion' + ' ' + i);
   }
-  return '' + Array;
+  return '' + array;
 }
 console.log(forLoop2());
 
 function forInLoop1() {
-  var Array = [];
+  const array = [];
   const object = {
     name: 'Tim Woo-Sam',
     age: '22',
@@ -47,14 +47,14 @@ function forInLoop1() {
     invention: 'Oreo and brownie cookies',
   };
   for (const property in object) {
-    Array.push(property);
+    array.push(property);
   }
-  return Array;
+  return array;
 }
 console.log('forInLoop1 output', forInLoop1());
 
 function forInLoop2(object) {
-  var Array = [];
+  const array = [];
   object = {
     name: 'Tim Woo-Sam',
     age: '22',
@@ -62,8 +62,8 @@ function forInLoop2(object) {
     invention: 'Oreo brookies',
   };
   for (const property in object) {
-    Array.push(object[property]);
+    array.push(object[property]);
   }
-  return Array;
+  return array;
 }
 console.log('forInLoop2 output', forInLoop2());
