83ca6240f2574d2bc211ea948e3333bde4c1ab88
Tim Woo-Sam <twoosam10@gmail.com>
2023-06-29 22:55:48 +0000

    Completed prep-array-challenge exercise.

---
+++ prep-array-challenge/src/compact.js
@@ -0,0 +1,11 @@
+/* exported compact */
+
+function compact(arr) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
